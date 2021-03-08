a10c90b5480f3d88bebf5e81ab6b652fbe0fe8ca
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-03-08 11:30:12 +0100

    Idk

--- Freelancing/Assets/Script/Celine/PlaceClicj.cs
+++ Freelancing/Assets/Script/Celine/PlaceClicj.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PlaceClicj : MonoBehaviour
 {
-    public GameObject Cube;
+    [SerializeField]
+    private GameObject Cube;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 mousePos;
 
+    [SerializeField]
+    private LayerMask allTilesLayer;
+          
     public void Spawn(Vector3 position)
     {
         Instantiate(Cube).transform.position = position;
@@ -21,13 +21,36 @@ public class PlaceClicj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+
+        //    Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, Cube.transform.position.z);
+
+        //    Spawn(adjustZ);
+        //}
+
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
-
-            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, Cube.transform.position.z);
-
-            Spawn(adjustZ);
+            Vector2 mouseRay = Camera.main.ScreenToWorldPoint(transform.position);
+            RaycastHit2D rayHit = Physics2D.Raycast(mouseRay, Vector2.zero, Mathf.Infinity, allTilesLayer);
+            if (rayHit.collider != null)
+            { 
+                if (rayHit.collider.gameObject.tag == "GrassTile" && this.gameObject.tag == "HouseTemplate")
+                {
+                    Instantiate(Cube, transform.position, Quaternion.identity);
+                }
+            }
+           
+            else if (rayHit.collider == null && this.gameObject.tag == "GrassTemplate")
+            {
+                Instantiate(Cube, transform.position, Quaternion.identity);
+            }
         }
+
     }
 }
