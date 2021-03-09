1f82ecd12eeefe2a5eb428b7f2ffecf3b2305939
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-03-09 10:53:23 +0100

    UIMetScript

---
+++ Freelancing/Assets/Script/UI/Startscreen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startscreen : MonoBehaviour
+{
+    public GameObject startScreenUI;
+    public GameObject settingsScreenUI;
+
+    private void Start()
+    {
+        startScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+
+    public void StartGame()
+    {
+        startScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        
+        settingsScreenUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        settingsScreenUI.SetActive(false);
+    }
+}
