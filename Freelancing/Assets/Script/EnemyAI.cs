5ab749503b9c677ffa926722fc81e16b167d1e5c
Simone <529336@student.glu.nl>
2021-03-26 17:01:05 +0100

    Ai things

--- Freelancing/Assets/Script/EnemyAI.cs
+++ Freelancing/Assets/Script/EnemyAI.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     private List<GameObject> m_CardList = new List<GameObject>();
-    private int m_CardInt;
+    private int m_CardIndex;
     private GameObject m_SelectedCardObject;
 
-    private int m_round;
+    private GameObject m_lastCard;
+    private GameObject m_lastGridPiece;
     private int m_cards;
 
     private void Start()
     {
-        m_round = 8;
         m_cards = 4;
     }
 
@@ -27,39 +27,56 @@ public class EnemyAI : MonoBehaviour
 
     public void PickACard()
     {
-        GameObject currentCard;
-
-        for (int i = 0; i < m_cards; i++)
+        if (m_cards < 1)
         {
-            currentCard = GameManager.instance.m_EnemyPiecesParent.transform.GetChild(i).gameObject;
+            m_lastCard = GameManager.instance.m_EnemyPiecesParent.transform.GetChild(0).gameObject;
 
-            m_CardList.Add(currentCard);
+            m_SelectedCardObject = m_lastCard;
+        }
+        else
+        {
+            for (int i = 0; i < m_cards; i++)
+            {
+                m_CardList.Add(GameManager.instance.m_EnemyPiecesParent.transform.GetChild(i).gameObject);
+            }
+
+            m_CardIndex = Random.Range(0, m_CardList.Count);
+
+            m_SelectedCardObject = m_CardList[m_CardIndex];
         }
 
-        m_CardInt = Random.Range(0, m_CardList.Count);
-        m_SelectedCardObject = m_CardList[m_CardInt];
-
-        PlaceCard();
-
-        m_cards--;
-        m_CardList.RemoveAt(m_CardInt);
+        if (m_SelectedCardObject != null)
+        {
+            PlaceCard();
+        }
     }
 
     public void PlaceCard()
     {
-        int fieldPieceInt = Random.Range(0, m_round);
-        GameObject currentFieldPiece = GameManager.instance.m_FieldPiecesParent.transform.GetChild(fieldPieceInt).gameObject;
+        int fieldPieceIndex = Random.Range(0, GridManager.instance.m_Grid.Length);
+
+        GameObject currentFieldPiece = GameManager.instance.m_FieldPiecesParent.transform.GetChild(fieldPieceIndex).gameObject;
 
         if (currentFieldPiece.CompareTag("FieldPieces"))
         {
-            GameManager.instance.m_SelectedPiece = currentFieldPiece;
+            if (currentFieldPiece.gameObject.transform.parent != null && m_SelectedCardObject.gameObject.transform.parent != null)
+            {
+                if (currentFieldPiece.gameObject.transform.parent.gameObject == GameManager.instance.m_FieldPiecesParent)
+                {
+                    if (currentFieldPiece != null)
+                    {
+                        GameManager.instance.m_SelectedPiece = m_SelectedCardObject;
 
-            GridManager.instance.SetGridPieceOnLocation(m_SelectedCardObject, GameManager.instance.m_SelectedPiece);
-            GameManager.instance.m_SelectedPiece = null;
+                        GridManager.instance.SetGridPieceOnLocation(GameManager.instance.m_SelectedPiece, currentFieldPiece);
+                        currentFieldPiece = null;
 
-            m_round--;
+                        GameManager.instance.SwitchTurn();
 
-            GameManager.instance.SwitchTurn();
+                        m_cards--;
+                        m_CardList.Clear();
+                    }
+                }
+            }
         }
         else
         {
